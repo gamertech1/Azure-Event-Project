404b7a07c45dae3617d52375712af15f87c5b5a1
Gamer Tech <gamertech996@gmail.com>
2026-04-26 06:37:10 +0000

    Updated terraform-script.sh

--- infra/scripts/terraform-script.sh
+++ infra/scripts/terraform-script.sh
@@ -4,6 +4,8 @@ set -e
 ACTION=$1
 ENV=$2
 COMPONENT=$3
+PLAN_FILE=$4
+
 if [ -z "$ACTION" ] || [ -z "$ENV" ]; then
   echo "Usage: $0 <plan|apply|destroy> <environment>"
   exit 1
@@ -19,7 +21,7 @@ terraform init \
 if [ "$ACTION" == "plan" ]; then
   terraform plan -out=tfplan
 elif [ "$ACTION" == "apply" ]; then
-  terraform apply -auto-approve tfplan
+  terraform apply -auto-approve "$PLAN_FILE"
 elif [ "$ACTION" == "destroy" ]; then
   terraform destroy -auto-approve
 fi
