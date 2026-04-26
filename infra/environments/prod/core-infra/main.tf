ec7f600554d5de7bf17cc73919ba3c6876d5e650
Gamer Tech <gamertech996@gmail.com>
2026-04-26 06:51:17 +0000

    Updated main.tf

--- infra/environments/prod/core-infra/main.tf
+++ infra/environments/prod/core-infra/main.tf
@@ -18,26 +18,25 @@ module "keyvault" {
   ssh_key_secret_name = "agent-pool-ssh-public-key"
 }
 
+module "vm" {
+  source              = "../../../modules/vm"
+  key_vault_id        = module.keyvault.keyvault-1
+  ssh_key_secret_name = module.keyvault.ssh_key_secret_name
+  depends_on          = [module.keyvault]
+  resource_group_name = module.rg.rg-1
+  location            = var.location
+  environment         = var.environment
+  org                 = var.org
+  project             = var.project
+}
 
-// module "vm" {
-//   source              = "../../../modules/vm"
-//   key_vault_id        = module.keyvault.keyvault-1
-//   ssh_key_secret_name = module.keyvault.ssh_key_secret_name
-//   depends_on          = [module.keyvault]
-//   resource_group_name = module.rg.rg-1
-//   location            = var.location
-//   environment         = var.environment
-//   org                 = var.org
-//   project             = var.project
-// }
-
-// module "nsg" {
-//   source      = "../../../modules/nsg"
-//   name        = "${var.prefix}-nsg"
-//   location    = var.location
-//   rg_name     = module.rg.rg-1
-//   subnet_id   = module.vm.subnet_id
-//   environment = var.environment
-//   org         = var.org
-//   project     = var.project
-// }
+module "nsg" {
+  source      = "../../../modules/nsg"
+  name        = "${var.prefix}-nsg"
+  location    = var.location
+  rg_name     = module.rg.rg-1
+  subnet_id   = module.vm.subnet_id
+  environment = var.environment
+  org         = var.org
+  project     = var.project
+}
