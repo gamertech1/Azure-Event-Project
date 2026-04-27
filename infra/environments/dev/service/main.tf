279f7b1a83004af839041782548ebea2fcbf79a0
Gamer Tech <gamertech996@gmail.com>
2026-04-27 00:23:28 +0000

    Updated main.tf

--- infra/environments/dev/service/main.tf
+++ infra/environments/dev/service/main.tf
@@ -7,19 +7,19 @@ module "rg" {
   project     = var.project
 }
 
-module "eventygrid" {
-  source              = "../../../modules/eventgrid"
-  location            = var.location
-  org                 = var.org
-  project             = var.project
-  environment         = var.environment
-  resource_group_name = module.rg.rg-1
-}
-module "servicebus" {
-  source              = "../../../modules/servicebus"
-  location            = var.location
-  org                 = var.org
-  project             = var.project
-  environment         = var.environment
-  resource_group_name = module.rg.rg-1
-}
+// module "eventygrid" {
+//   source              = "../../../modules/eventgrid"
+//   location            = var.location
+//   org                 = var.org
+//   project             = var.project
+//   environment         = var.environment
+//   resource_group_name = module.rg.rg-1
+// }
+// module "servicebus" {
+//   source              = "../../../modules/servicebus"
+//   location            = var.location
+//   org                 = var.org
+//   project             = var.project
+//   environment         = var.environment
+//   resource_group_name = module.rg.rg-1
+// }
